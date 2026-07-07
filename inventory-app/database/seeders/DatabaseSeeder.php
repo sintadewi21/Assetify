ac5b47cd45e5e496f814daf76adc93f99bc1fe24
sintadewi21 <sint.adr21@gmail.com>
2026-07-08 01:21:44 +0700

    chore: make DatabaseSeeder idempotent using firstOrCreate

--- inventory-app/database/seeders/DatabaseSeeder.php
+++ inventory-app/database/seeders/DatabaseSeeder.php
@@ -14,47 +14,57 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // 1. Buat Role
-        $adminRole = Role::create(['name' => 'Admin']);
-        $staffRole = Role::create(['name' => 'Staff']);
-        $managerRole = Role::create(['name' => 'Manager']);
+        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
+        $staffRole = Role::firstOrCreate(['name' => 'Staff']);
+        $managerRole = Role::firstOrCreate(['name' => 'Manager']);
 
         // 2. Buat Akun Login Testing
         // 3 Admin Accounts
-        User::create([
-            'name' => 'Admin1',
-            'email' => 'admin1@gmail.com',
-            'password' => bcrypt('admin123'),
-            'role' => 'admin',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin1@gmail.com'],
+            [
+                'name' => 'Admin1',
+                'password' => bcrypt('admin123'),
+                'role' => 'admin',
+            ]
+        );
 
-        User::create([
-            'name' => 'Admin2',
-            'email' => 'admin2@gmail.com',
-            'password' => bcrypt('admin123'),
-            'role' => 'admin',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin2@gmail.com'],
+            [
+                'name' => 'Admin2',
+                'password' => bcrypt('admin123'),
+                'role' => 'admin',
+            ]
+        );
 
-        User::create([
-            'name' => 'Admin3',
-            'email' => 'admin3@gmail.com',
-            'password' => bcrypt('admin123'),
-            'role' => 'admin',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin3@gmail.com'],
+            [
+                'name' => 'Admin3',
+                'password' => bcrypt('admin123'),
+                'role' => 'admin',
+            ]
+        );
 
         // 1 Staff Account
-        User::create([
-            'name' => 'Staff Inventory',
-            'email' => 'staff@gmail.com',
-            'password' => bcrypt('staff123'),
-            'role' => 'staff',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'staff@gmail.com'],
+            [
+                'name' => 'Staff Inventory',
+                'password' => bcrypt('staff123'),
+                'role' => 'staff',
+            ]
+        );
 
         // 1 Manager Account
-        User::create([
-            'name' => 'Manager Inventory',
-            'email' => 'manager@gmail.com',
-            'password' => bcrypt('manager123'),
-            'role' => 'manager',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'manager@gmail.com'],
+            [
+                'name' => 'Manager Inventory',
+                'password' => bcrypt('manager123'),
+                'role' => 'manager',
+            ]
+        );
     }
 }
